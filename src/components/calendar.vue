daf0d38b224bc68966d303d699a915d6014677c6
devdoodan <devdoodan@gmail.com>
2020-11-19 14:14:46 +0530

    updated veutify calendar

--- src/components/calendar.vue
+++ src/components/calendar.vue
@@ -51,14 +51,28 @@
           ref="calendar"
           v-model="focus"
           color="primary"
+          :type="type"
           :events="events"
           :event-color="getEventColor"
-          :type="type"
+          :event-ripple="false"
+          @change="getEvents"
           @click:event="showEvent"
           @click:more="viewDay"
-          @click:date="viewDay"
-          @change="updateRange"
-        ></v-calendar>
+          @mousedown:event="startDrag"
+          @mousedown:time="startTime"
+          @mousemove:time="mouseMove"
+          @mouseup:time="endDrag"
+          @mouseleave.native="cancelDrag"
+        >
+          <template v-slot:event="{ event, timed, eventSummary }">
+            <div class="v-event-draggable" v-html="eventSummary()"></div>
+            <div
+              v-if="timed"
+              class="v-event-drag-bottom"
+              @mousedown.stop="extendBottom(event)"
+            ></div>
+          </template>
+        </v-calendar>
         <v-menu
           v-model="selectedOpen"
           :close-on-content-click="false"
@@ -96,26 +110,24 @@
 <script>
 export default {
   data: () => ({
+    value: "",
+    events: [],
+    type: "week",
     focus: "",
-    type: "month",
     typeToLabel: {
       month: "Month",
       week: "Week",
       day: "Day",
       "4day": "4 Days",
     },
-    selectedEvent: {},
-    selectedElement: null,
-    selectedOpen: false,
-    events: [],
     colors: [
-      "blue",
-      "indigo",
-      "deep-purple",
-      "cyan",
-      "green",
-      "orange",
-      "grey darken-1",
+      "#2196F3",
+      "#3F51B5",
+      "#673AB7",
+      "#00BCD4",
+      "#4CAF50",
+      "#FF9800",
+      "#757575",
     ],
     names: [
       "Meeting",
@@ -127,18 +139,21 @@ export default {
       "Conference",
       "Party",
     ],
+    selectedEvent: {},
+    selectedElement: null,
+    selectedOpen: false,
+    dragEvent: null,
+    dragStart: null,
+    createEvent: null,
+    createStart: null,
+    extendOriginal: null,
   }),
-  mounted() {
-    this.$refs.calendar.checkChange();
-  },
   methods: {
     viewDay({ date }) {
       this.focus = date;
       this.type = "day";
     },
-    getEventColor(event) {
-      return event.color;
-    },
+
     setToday() {
       this.focus = "";
     },
@@ -166,27 +181,134 @@ export default {
 
       nativeEvent.stopPropagation();
     },
-    updateRange({ start, end }) {
+    startDrag({ event, timed }) {
+      if (event && timed) {
+        this.dragEvent = event;
+        this.dragTime = null;
+        this.extendOriginal = null;
+      }
+    },
+    startTime(tms) {
+      const mouse = this.toTime(tms);
+
+      if (this.dragEvent && this.dragTime === null) {
+        const start = this.dragEvent.start;
+
+        this.dragTime = mouse - start;
+      } else {
+        this.createStart = this.roundTime(mouse);
+        this.createEvent = {
+          name: `Event #${this.events.length}`,
+          color: this.rndElement(this.colors),
+          start: this.createStart,
+          end: this.createStart,
+          timed: true,
+        };
+
+        this.events.push(this.createEvent);
+      }
+    },
+    extendBottom(event) {
+      this.createEvent = event;
+      this.createStart = event.start;
+      this.extendOriginal = event.end;
+    },
+    mouseMove(tms) {
+      const mouse = this.toTime(tms);
+
+      if (this.dragEvent && this.dragTime !== null) {
+        const start = this.dragEvent.start;
+        const end = this.dragEvent.end;
+        const duration = end - start;
+        const newStartTime = mouse - this.dragTime;
+        const newStart = this.roundTime(newStartTime);
+        const newEnd = newStart + duration;
+
+        this.dragEvent.start = newStart;
+        this.dragEvent.end = newEnd;
+      } else if (this.createEvent && this.createStart !== null) {
+        const mouseRounded = this.roundTime(mouse, false);
+        const min = Math.min(mouseRounded, this.createStart);
+        const max = Math.max(mouseRounded, this.createStart);
+
+        this.createEvent.start = min;
+        this.createEvent.end = max;
+      }
+    },
+    endDrag() {
+      this.dragTime = null;
+      this.dragEvent = null;
+      this.createEvent = null;
+      this.createStart = null;
+      this.extendOriginal = null;
+    },
+    cancelDrag() {
+      if (this.createEvent) {
+        if (this.extendOriginal) {
+          this.createEvent.end = this.extendOriginal;
+        } else {
+          const i = this.events.indexOf(this.createEvent);
+          if (i !== -1) {
+            this.events.splice(i, 1);
+          }
+        }
+      }
+
+      this.createEvent = null;
+      this.createStart = null;
+      this.dragTime = null;
+      this.dragEvent = null;
+    },
+    roundTime(time, down = true) {
+      const roundTo = 15; // minutes
+      const roundDownTime = roundTo * 60 * 1000;
+
+      return down
+        ? time - (time % roundDownTime)
+        : time + (roundDownTime - (time % roundDownTime));
+    },
+    toTime(tms) {
+      return new Date(
+        tms.year,
+        tms.month - 1,
+        tms.day,
+        tms.hour,
+        tms.minute
+      ).getTime();
+    },
+    getEventColor(event) {
+      const rgb = parseInt(event.color.substring(1), 16);
+      const r = (rgb >> 16) & 0xff;
+      const g = (rgb >> 8) & 0xff;
+      const b = (rgb >> 0) & 0xff;
+
+      return event === this.dragEvent
+        ? `rgba(${r}, ${g}, ${b}, 0.7)`
+        : event === this.createEvent
+        ? `rgba(${r}, ${g}, ${b}, 0.7)`
+        : event.color;
+    },
+    getEvents({ start, end }) {
       const events = [];
 
-      const min = new Date(`${start.date}T00:00:00`);
-      const max = new Date(`${end.date}T23:59:59`);
-      const days = (max.getTime() - min.getTime()) / 86400000;
+      const min = new Date(`${start.date}T00:00:00`).getTime();
+      const max = new Date(`${end.date}T23:59:59`).getTime();
+      const days = (max - min) / 86400000;
       const eventCount = this.rnd(days, days + 20);
 
       for (let i = 0; i < eventCount; i++) {
-        const allDay = this.rnd(0, 3) === 0;
-        const firstTimestamp = this.rnd(min.getTime(), max.getTime());
-        const first = new Date(firstTimestamp - (firstTimestamp % 900000));
-        const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000;
-        const second = new Date(first.getTime() + secondTimestamp);
+        const timed = this.rnd(0, 3) !== 0;
+        const firstTimestamp = this.rnd(min, max);
+        const secondTimestamp = this.rnd(2, timed ? 8 : 288) * 900000;
+        const start = firstTimestamp - (firstTimestamp % 900000);
+        const end = start + secondTimestamp;
 
         events.push({
-          name: this.names[this.rnd(0, this.names.length - 1)],
-          start: first,
-          end: second,
-          color: this.colors[this.rnd(0, this.colors.length - 1)],
-          timed: !allDay,
+          name: this.rndElement(this.names),
+          color: this.rndElement(this.colors),
+          start,
+          end,
+          timed,
         });
       }
 
@@ -195,6 +317,46 @@ export default {
     rnd(a, b) {
       return Math.floor((b - a + 1) * Math.random()) + a;
     },
+    rndElement(arr) {
+      return arr[this.rnd(0, arr.length - 1)];
+    },
   },
 };
 </script>
+
+<style scoped lang="scss">
+.v-event-draggable {
+  padding-left: 6px;
+}
+
+.v-event-timed {
+  user-select: none;
+  -webkit-user-select: none;
+}
+
+.v-event-drag-bottom {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 4px;
+  height: 4px;
+  cursor: ns-resize;
+
+  &::after {
+    display: none;
+    position: absolute;
+    left: 50%;
+    height: 4px;
+    border-top: 1px solid white;
+    border-bottom: 1px solid white;
+    width: 16px;
+    margin-left: -8px;
+    opacity: 0.8;
+    content: "";
+  }
+
+  &:hover::after {
+    display: block;
+  }
+}
+</style>
